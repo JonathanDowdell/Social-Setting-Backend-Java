3be5d3b37627350bc34f0e2aaf14f38c3431f924
jonathandowdell <8318Pro2711>
2020-12-27 19:03:38 -0600

    Fixes: + SubSetting

--- src/main/java/com/mettaworldj/socialsetting/server/service/subSetting/ISubSettingService.java
+++ src/main/java/com/mettaworldj/socialsetting/server/service/subSetting/ISubSettingService.java
@@ -1,15 +1,13 @@
 package com.mettaworldj.socialsetting.server.service.subSetting;
 
-import com.mettaworldj.socialsetting.server.dto.post.reponse.PostResponseDto;
 import com.mettaworldj.socialsetting.server.dto.subSetting.request.SubSettingRequestDto;
+import com.mettaworldj.socialsetting.server.dto.subSetting.response.SubSettingFeedResponseDto;
 import com.mettaworldj.socialsetting.server.dto.subSetting.response.SubSettingResponseDto;
-
-import java.util.List;
 
 public interface ISubSettingService {
 
     SubSettingResponseDto createSubSetting(SubSettingRequestDto subSettingRequestDto);
-    List<PostResponseDto> getSubSettingFeedByName(String subSettingName, int page, int amount, boolean profileInfo);
+    SubSettingFeedResponseDto getSubSettingFeedByName(String subSettingName, int page, int amount, boolean profileInfo);
     void followSubSettingByName(String subSettingName);
     void unfollowSubSettingByName(String subSettingName);
 }
